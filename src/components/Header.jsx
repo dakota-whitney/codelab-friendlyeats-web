df2ff31fa0871cf4faa590907e6afbabd98bb585
Dakota Whitney <whitneydakota@gmail.com>
2025-10-13 20:08:21 -0400

    Read the list of restaurants from Firestore

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,31 +4,10 @@ import Link from "next/link";
 import {
   signInWithGoogle,
   signOut,
-  // onIdTokenChanged,
   onAuthStateChanged,
 } from "@/src/lib/firebase/auth.js";
 import { addFakeRestaurantsAndReviews } from "@/src/lib/firebase/firestore.js";
 import { setCookie, deleteCookie } from "cookies-next";
-
-// function useUserSession(initialUser) {
-  
-//   useEffect(() => {
-//     return onIdTokenChanged(async user => {
-//       if(user){
-//         const idToken = await user.getIdToken();
-//         await setCookie("___session", idToken);
-//         window.location.reload();
-//       } else {
-//         return await deleteCookie("__session");
-//       };
-
-//       // if(initialUser?.uid === user?.uid) return;
-//       // else window.location.reload();
-//     });
-//   }, [initialUser]);
-
-//   return initialUser;
-// };
 
 function UserHeader({ user }) {
 
@@ -92,24 +71,11 @@ export default function Header({ initialUser }) {
 
   useEffect(() => {
     return onAuthStateChanged(async user => {
-      if(!user) return await deleteCookie("__session");
-      const idToken = await user.getIdToken();
-      await setCookie("___session", idToken);
+      if(!user) await deleteCookie("__session");
+      else await user.getIdToken().then(token => setCookie("___session", token))
       setUser(user);
     });
   }, []);
-
-  // const user = useUserSession(initialUser);
-
-  // const handleSignOut = (event) => {
-  //   event.preventDefault();
-  //   signOut();
-  // };
-
-  // const handleSignIn = (event) => {
-  //   event.preventDefault();
-  //   signInWithGoogle();
-  // };
 
   return (
     <header>
@@ -118,46 +84,6 @@ export default function Header({ initialUser }) {
         Friendly Eats
       </Link>
       {user ? <UserHeader user={user} /> : <NoUserHeader />}
-      {/* {user ? (
-        <>
-          <div className="profile">
-            <p>
-              <img
-                className="profileImage"
-                src={user.photoURL || "/profile.svg"}
-                alt={user.email}
-              />
-              {user.displayName}
-            </p>
-
-            <div className="menu">
-              ...
-              <ul>
-                <li>{user.displayName}</li>
-
-                <li>
-                  <a href="#" onClick={addFakeRestaurantsAndReviews}>
-                    Add sample restaurants
-                  </a>
-                </li>
-
-                <li>
-                  <a href="#" onClick={handleSignOut}>
-                    Sign Out
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </>
-      ) : (
-        <div className="profile">
-          <a href="#" onClick={handleSignIn}>
-            <img src="/profile.svg" alt="A placeholder user image" />
-            Sign In with Google
-          </a>
-        </div>
-      )} */}
     </header>
   );
 };
