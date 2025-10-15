8a431be087c95ce131a30f8bb2b9ea43dfc4070b
Dakota Whitney <whitneydakota@gmail.com>
2025-10-15 19:41:23 -0400

    Remove config.js

--- src/lib/firebase/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_TEST_API_KEY,
-    authDomain: "friendlyeats-codelab-46f69.firebaseapp.com",
-    projectId: "friendlyeats-codelab-46f69",
-    storageBucket: "friendlyeats-codelab-46f69.firebasestorage.app",
-    messagingSenderId: "142938368131",
-    appId: "1:142938368131:web:ae0c8daa2ec376bebdd3b6"
-};
